17a1eb9ae4586f1f74670d9a0418c06463923073
Yao Bin Then <alvinthen890703@gmail.com>
2016-06-26 00:26:34 +0800

    Add react-native-maps

--- js/containers/Place.js
+++ js/containers/Place.js
@@ -12,7 +12,16 @@ import MapView from 'react-native-maps';
 class Place extends React.Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
         <Text>{this.props.name}</Text>
       </View>
     );
@@ -26,46 +35,22 @@ Place.propTypes = {
 
 const styles = createStyleSheet({
   container: {
-    flex: 1,
-    backgroundColor: '#fafafa',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  bottomContainer: {
-    flex: 1,
-    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 400,
+    width: 400,
     justifyContent: 'flex-end',
-  },
-  button: {
-    height: 150,
-    width: 150,
     alignItems: 'center',
-    backgroundColor: '#37474f',
-    borderRadius: 150,
-    margin: 16,
-    padding: 8,
-    elevation: 2,
-    shadowColor: 'grey',
-    shadowRadius: 2,
-    shadowOpacity: 0.7,
-    shadowOffset: {
-      width: 2,
-      height: 2,
-    },
-  },
-  buttonText: {
-    flex: 1,
-    color: 'white',
-    fontSize: 14,
-    fontWeight: '500',
   },
   map: {
     position: 'absolute',
+    top: 0,
     left: 0,
     right: 0,
-    top: 0,
     bottom: 0,
-    height: 300,
   },
 });
 
