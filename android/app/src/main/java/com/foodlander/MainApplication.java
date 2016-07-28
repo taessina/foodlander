ba78cfbae928078483c7b57885d33d069b8b0cf3
Yao Bin Then <alvinthen890703@gmail.com>
2016-07-28 16:50:14 +0800

    Remove unused dependencies

--- android/app/src/main/java/com/foodlander/MainApplication.java
+++ android/app/src/main/java/com/foodlander/MainApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.react.rnspinkit.RNSpinkitPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -27,8 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNSpinkitPackage(),
             new MapsPackage()
       );
     }
