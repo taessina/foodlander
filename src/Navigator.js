c1e3a30d0febf3bdaaba2455e40798ec93ac8287
V-SHY <vshy.ho@taessina.com>
2018-04-22 23:04:12 +0800

    ⬆️ Upgrade packages
    Add prettier related packages and configuration
    Add react-navigation-redux-helpers for redux-navigation
    Solve the yarn test error but still have react-error-boundaries error message

--- src/Navigator.js
+++ src/Navigator.js
@@ -3,18 +3,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { addNavigationHelpers, StackNavigator, type NavigationState } from 'react-navigation';
 import Home from './containers/Home';
+import { addListener } from './redux/react-navigation';
 
-export const Navigator = StackNavigator({
-  Home: { screen: Home },
-}, { headerMode: 'none' });
+export const Navigator = StackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  { headerMode: 'none' },
+);
 
 type Props = { dispatch: Function, nav: NavigationState };
 
 const NavigatorWithState = (props: Props) => (
-  <Navigator navigation={addNavigationHelpers({
-    dispatch: props.dispatch,
-    state: props.nav,
-  })}
+  <Navigator
+    navigation={addNavigationHelpers({
+      dispatch: props.dispatch,
+      state: props.nav,
+      addListener,
+    })}
   />
 );
 
--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -7,8 +7,9 @@ import { middleware as reduxPackMiddleware } from 'redux-pack';
 import { createLogger } from 'redux-logger';
 
 import reducers, { blacklist } from './modules';
+import { middleware as reactNavigationReduxMiddleware } from './react-navigation';
 
-const isDebuggingInChrome = __DEV__ && !!window.navigator.userAgent;
+const isDebuggingInChrome = __DEV__ && !!(window && window.navigator && window.navigator.userAgent);
 
 const logger = createLogger({
   predicate: () => isDebuggingInChrome,
@@ -23,7 +24,14 @@ const config = {
 };
 
 export default function configureStore() {
-  const enhancer = compose(applyMiddleware(thunk, reduxPackMiddleware, logger));
+  let enhancer;
+  if (__DEV__) {
+    enhancer = compose(
+      applyMiddleware(reactNavigationReduxMiddleware, thunk, reduxPackMiddleware, logger)
+    );
+  } else {
+    enhancer = compose(applyMiddleware(reactNavigationReduxMiddleware, thunk, reduxPackMiddleware));
+  }
   const store = createStore(persistCombineReducers(config, reducers), enhancer);
   const persistor = persistStore(store);
   // persistor.purge();
